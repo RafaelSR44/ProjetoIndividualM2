d27a3db32a85325023ce486bdcf54c5f7e1b3242
Rafael Santana Rodrigues <138138154+RafaelSR44@users.noreply.github.com>
2025-05-26 09:55:37 -0300

    refactor: alteracao bcryptjs

--- server.js
+++ server.js
@@ -55,9 +55,6 @@ app.use((err, req, res, next) => {
 
 // Inicializa o servidor
 const PORT = process.env.PORT || 3000;
-
-if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => {
     console.log(`Servidor rodando na porta http://localhost:${PORT}`);
   });
-}
